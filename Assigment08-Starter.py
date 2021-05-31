a9260e341d7980dc4c845abd84f9602a83da4f15
uwferenczy <83683285+uwferenczy@users.noreply.github.com>
2021-05-31 06:04:18 -0700

    Adding initialized project assignment 08

---
+++ Assigment08-Starter.py
@@ -0,0 +1,69 @@
+# ------------------------------------------------------------------------ #
+# Title: Assignment 08
+# Description: Working with classes
+
+# ChangeLog (Who,When,What):
+# RRoot,1.1.2030,Created started script
+# RRoot,1.1.2030,Added pseudo-code to start assignment 8
+# <Your Name>,<Today's Date>,Modified code to complete assignment 8
+# ------------------------------------------------------------------------ #
+
+# Data -------------------------------------------------------------------- #
+strFileName = 'products.txt'
+lstOfProductObjects = []
+
+class Product:
+    """Stores data about a product:
+
+    properties:
+        product_name: (string) with the products's  name
+        product_price: (float) with the products's standard price
+    methods:
+    changelog: (When,Who,What)
+        RRoot,1.1.2030,Created Class
+        <Your Name>,<Today's Date>,Modified code to complete assignment 8
+    """
+    pass
+    # TODO: Add Code to the Product class
+# Data -------------------------------------------------------------------- #
+
+# Processing  ------------------------------------------------------------- #
+class FileProcessor:
+    """Processes data to and from a file and a list of product objects:
+
+    methods:
+        save_data_to_file(file_name, list_of_product_objects):
+
+        read_data_from_file(file_name): -> (a list of product objects)
+
+    changelog: (When,Who,What)
+        RRoot,1.1.2030,Created Class
+        <Your Name>,<Today's Date>,Modified code to complete assignment 8
+    """
+    pass
+    # TODO: Add Code to process data from a file
+    # TODO: Add Code to process data to a file
+
+# Processing  ------------------------------------------------------------- #
+
+# Presentation (Input/Output)  -------------------------------------------- #
+class IO:
+    # TODO: Add docstring
+    pass
+    # TODO: Add code to show menu to user
+    # TODO: Add code to get user's choice
+    # TODO: Add code to show the current data from the file to user
+    # TODO: Add code to get product data from user
+# Presentation (Input/Output)  -------------------------------------------- #
+
+# Main Body of Script  ---------------------------------------------------- #
+# TODO: Add Data Code to the Main body
+# Load data from file into a list of product objects when script starts
+# Show user a menu of options
+# Get user's menu option choice
+    # Show user current data in the list of product objects
+    # Let user add data to the list of product objects
+    # let user save current data to file and exit program
+
+# Main Body of Script  ---------------------------------------------------- #
+
